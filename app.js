55d45f33107c39cca26689d5f094c01d45fa845c
Nicholas Peters <95872411+Redicholas@users.noreply.github.com>
2022-11-14 11:54:11 +0100

    Lint och Prettier försök 2

--- app.js
+++ app.js
@@ -101,24 +101,25 @@ function renderProducts() {
                 <p>${products[i].desc}</p>
                     <div class="product-selection">
                         <p>${products[i].price}</p>
-                        <button>Köp</button>
-                        <select name="amount">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        </select>
+                        <form>
+                            <input type="submit">Köp</input>
+                            <select name="amount">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
+                        </form>
                     </div>
-                </div>
             </div>
         </div>
-        ` 
+        `
     }
 }
 
